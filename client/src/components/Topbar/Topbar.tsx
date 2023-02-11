201539f254552bb829e106aab8c01c39925b462f
Alex <acal32@berkeley.edu>
2023-02-10 20:30:09 -0800

    First comp

--- client/src/components/Topbar/Topbar.tsx
+++ client/src/components/Topbar/Topbar.tsx
@@ -1,81 +1,54 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
-import MenuItem from "@mui/material/MenuItem";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Box from '@mui/material/Box';
 import {useState} from 'react';
-
-const pages = ["Revenue","Expenses","Invoices","Receipts"];
 
 const Topbar = () => {
 
-  const [navAnchor,setnavAnchor] = useState<null|HTMLElement>(
-    null);
-  
-  const handleOpenMenu = (event:React.MouseEvent<HTMLElement>) => {
-    setnavAnchor(event.currentTarget);
-  };
+  const [anchorEl,setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
 
-  const handleCloseMenu = (event:React.MouseEvent<HTMLElement>) => {
-    setnavAnchor(null);
+  const handleClick = (event:React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
   }
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  }
+
   return (
-    <AppBar position="static">
-      <Container maxWidth='xl'>
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none"
-            }}
-          >
-          LOGO
-          </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton 
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenMenu}
-              color="inherit">
-                <MenuIcon/>
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={navAnchor}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left"
-                }}
-                keepMounted
-                transformOrigin={{
-                vertical: "top",
-                horizontal: "left"
-                }}
-                open={Boolean(navAnchor)}
-                >
-              </Menu>
-            </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <div>
+      <Button
+       id='basic-button'
+       aria-controls={open ? 'basic-menu' : undefined}
+       ara-haspopup="true"
+       aria-expanded={open ? 'true': undefined}
+       onClick={handleClick}
+       size="large"
+        >
+        Dashboard
+      </Button>
+      <Menu
+       anchorEl={anchorEl}
+       open={open}
+       onClose={handleClose}
+       MenuListProps={{
+        'aria-labelledby':'basic-button',
+       }}
+       >
+        <MenuItem onClick={handleClose}>Revenue</MenuItem>
+        <MenuItem onClick={handleClose}>Expenses</MenuItem>
+        <MenuItem onClick={handleClose}>Reciepts</MenuItem>
+        <MenuItem onClick={handleClose}>Invoices</MenuItem>
+      </Menu>
+
+    </div>
   )
 }
 
-export default Topbar
+export default Topbar;
+
+
+
