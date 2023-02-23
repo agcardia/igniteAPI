8469d659c0447eaa6a298e707b69613f8cbb8c65
Alex <acal32@berkeley.edu>
2023-02-22 20:44:44 -0800

    Added final icons

--- client/src/components/Topbar/Topbar.tsx
+++ client/src/components/Topbar/Topbar.tsx
@@ -6,8 +6,7 @@ import React from 'react';
 import {Outlet} from 'react-router-dom';
 import './Topbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faHouse} from '@fortawesome/free-solid-svg-icons';
-import {faFileInvoiceDollar} from '@fortawesome/free-solid-svg-icons';
+import {faHouse, faFileInvoiceDollar, faReceipt, faMoneyBillTrendUp, faCreditCard} from '@fortawesome/free-solid-svg-icons';
 
 const Topbar = () => {
   return (
@@ -15,19 +14,31 @@ const Topbar = () => {
         <Navbar className="navbar">
             <Container>
                 <Nav.Link href="/" className="homeitem"><FontAwesomeIcon icon={faHouse} /> Home</Nav.Link>
-                <NavDropdown title="Revenue" id="basic-nav-dropdown" className="navitem">
+                <NavDropdown 
+                title={<span><FontAwesomeIcon icon={faMoneyBillTrendUp} /> Revenue</span>} 
+                id="basic-nav-dropdown" className="navitem"
+                >
                     <NavDropdown.Item href="/revenue/add">Add Revenues</NavDropdown.Item>
                     <NavDropdown.Item href="/revenue/view">View Revenues</NavDropdown.Item>
                 </NavDropdown>
-                <NavDropdown title="Expense" id="basic-nav-dropdown" className="navitem">
+                <NavDropdown 
+                title={<span><FontAwesomeIcon icon={faCreditCard} /> Expenses</span>} 
+                id="basic-nav-dropdown" className="navitem"
+                >
                     <NavDropdown.Item href="/expense/add">Add Expenses</NavDropdown.Item>
                     <NavDropdown.Item href="/expense/view">View Expenses</NavDropdown.Item>
                 </NavDropdown>
-                <NavDropdown title={<span><FontAwesomeIcon icon={faFileInvoiceDollar} /> Invoices</span>} id="basic-nav-dropdown" className="navitem">
+                <NavDropdown 
+                title={<span><FontAwesomeIcon icon={faFileInvoiceDollar} /> Invoices</span>} 
+                id="basic-nav-dropdown" className="navitem"
+                >
                     <NavDropdown.Item href="/invoice/add">Add Invoice</NavDropdown.Item>
                     <NavDropdown.Item href="/invoice/view">View Invoices</NavDropdown.Item>
                 </NavDropdown>
-                <NavDropdown title="Reciept" id="basic-nav-dropdown" className="navitem">
+                <NavDropdown 
+                title={<span><FontAwesomeIcon icon={faReceipt} /> Reciepts</span>} 
+                id="basic-nav-dropdown" className="navitem"
+                >
                     <NavDropdown.Item href="/reciept/add">Add Reciept</NavDropdown.Item>
                     <NavDropdown.Item href="/reciept/view">View Reciepts</NavDropdown.Item>
                 </NavDropdown>
